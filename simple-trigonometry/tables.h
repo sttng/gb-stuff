37b55635369ba10391df29096e8a10a6f3758ae4
sttng <46765778+sttng@users.noreply.github.com>
2021-03-27 13:36:44 +0800

    Update tables.h

--- simple-trigonometry/tables.h
+++ simple-trigonometry/tables.h
@@ -1,5 +1,5 @@
 // Effective size is 1280.
-extern const fixed_t finesine[1280];
+extern const INT16 finesine[1280]; //need to change from const INT16 to const fixed_t later
 
 // Re-use data, is just PI/2 phase shift.
-static const fixed_t *const finecosine = finesine + (FINEANGLES/4);
+static const INT16 *const finecosine = finesine + (FINEANGLES/4);
