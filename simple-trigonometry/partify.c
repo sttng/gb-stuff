9a096e8ff5b1cf7abde78b3f7b364d556227c16e
sttng <46765778+sttng@users.noreply.github.com>
2021-03-28 17:21:39 +0800

    Update partify.c

--- simple-trigonometry/partify.c
+++ simple-trigonometry/partify.c
@@ -5,13 +5,14 @@ void partify(UINT16);
 
 int main()
 {
-  // watch out - numbers are represented using 2's complement!
+  // watch out - numbers should be represented using 2's complement!
   // so 0x1000 in an 8bit system is -32768 and 0x1001 is -32767
-  partify(0xff00);
-  //partify(0x7fff0000);
-  //partify(0x80010000);
-  //partify(0x0000ffff);
-  //partify(0x0001000f);
+  //partify(0xff00);
+  //partify(0x7f00);
+  //partify(0x8100);
+  //partify(0x00ff);
+  //partify(0x010f);
+  partify(0x81ff);
   return 0;
 }
 
@@ -19,6 +20,10 @@ void partify(UINT16 k)
 {
   printf("hexadecimal:%x \t\n", k);
   printf("unsigned int:%u \t\n", k);
-  printf("integer part:%d \t\n", k >> 8);
-  printf("floating part:%u \t\n", k & 0x00ff);
+  INT16 l = k;
+  printf("signed int:%d \t\n", l);
+  printf("int part (u):%d \t\n", k >> 8);
+  INT8 m = k >> 8;
+  printf("int part (s):%d \t\n", m);
+  printf("float part:%u \t\n", k & 0x00ff);
 }
