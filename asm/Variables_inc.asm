1760565594117fc95fdb016e4cf652ff0e1cdeb5
sttng <46765778+sttng@users.noreply.github.com>
2025-03-07 23:36:16 +0800

    Update Variables_inc.asm

--- asm/Variables_inc.asm
+++ asm/Variables_inc.asm
@@ -10,3 +10,15 @@ section "Variables",wram0,align[8]
 VariableRegion1 equ TopEdgeClip + 96
 VariableRegion2 equ BottomEdgeClip + 96
 
+;.module BlockMap
+
+DEF Header = 10      ;Header = AllocateVariable(0)
+DEF Origin_X = $FFFF ;Origin.X = AllocateVariable(2)
+DEF Origin_Y = $EEEE ;Origin.Y = AllocateVariable(2)
+DEF Width = 12 ;Width = AllocateVariable(1)
+DEF Height = 14 ;Height = AllocateVariable(1)
+DEF Header_Size EQU 7
+
+DEF Grid = $AAAA 
+
+;.endmodule
