b0f52c5e5c3b18f2624b976ea6af91ca66b6e8a7
sttng <46765778+sttng@users.noreply.github.com>
2025-03-08 01:59:17 +0800

    Update Variables_inc.asm

--- asm/Variables_inc.asm
+++ asm/Variables_inc.asm
@@ -10,13 +10,14 @@ section "Variables",wram0,align[8]
 VariableRegion1 equ TopEdgeClip + 96
 VariableRegion2 equ BottomEdgeClip + 96
 
-section "BlockMap",wram0 ;.module BlockMap
+section "BlockMapVars",wram0 ;.module BlockMap
 
 Header:        ;Header = AllocateVariable(0)
 Origin_X:    dw   ;Origin.X = AllocateVariable(2)
 Origin_Y:    dw   ;Origin.Y = AllocateVariable(2)
 Width:       db   ;Width = AllocateVariable(1)
 Height:      db   ;Height = AllocateVariable(1)
-Header_Size: db  7
 Grid:        dw
+def Header_Size equ Grid - Header
+
 ;.endmodule
