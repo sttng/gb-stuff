bf99835f0ab0b5ea9d87cc9415823a86620f4da7
sttng <46765778+sttng@users.noreply.github.com>
2025-03-07 17:53:59 +0800

    Update BlockMap.asm

--- asm/BlockMap.asm
+++ asm/BlockMap.asm
@@ -8,9 +8,30 @@ Code:
 ; Destroyed: AF, BC, DE, HL.
 ; ==========================================================================
 IntialiseFromLevel:
-	ld hl,(Level.BlockMap)
+	ld hl,[Level.BlockMap]
 	ld de,Header
 	ld bc,Header.Size
 	ldir					;copy from HL to DE with amount of BC
-	ld (Grid),hl
+	ld [Grid],hl
 	ret
+
+
+
+; ==========================================================================
+; ldir - Copies a specified memory-location to another location
+; Parameters:
+; hl: Source
+; de: Destination
+; bc: Count
+; ==========================================================================
+ldir:
+:	ld a,[hl]
+	inc hl
+	ld [de],a
+	inc de
+	dec bc
+	ld a,c
+	or a,b
+	jr nz, :-
+ret
+
