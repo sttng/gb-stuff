4ae6c32063cefbe942e6fe910a1cdb466ee1fb1d
sttng <46765778+sttng@users.noreply.github.com>
2025-03-07 20:36:25 +0800

    Create Level.asm

---
+++ asm/Level.asm
@@ -0,0 +1,1804 @@
+SECTION "Level", ROM0
+
+Vertices:
+	dw 0, -3072
+	dw 512, -3584
+	dw 512, -4096
+	dw 1280, -4096
+	dw 1280, -3584
+	dw 1792, -3072
+	dw 2048, -3072
+	dw 3072, -3072
+	dw 3328, -3072
+	dw 3840, -3584
+	dw 3840, -4096
+	dw 4608, -4096
+	dw 4608, -3584
+	dw 5120, -3072
+	dw 5120, -512
+	dw 4608, 0
+	dw 3072, 0
+	dw 2048, 0
+	dw 512, 0
+	dw 0, -512
+	dw 2048, -1536
+	dw 3072, -1536
+	dw 2048, -1024
+	dw 3072, -1024
+	dw 2048, -512
+	dw 3072, -512
+	dw -512, -7680
+	dw 1536, -7680
+	dw 1536, -7168
+	dw 3584, -7168
+	dw 3584, -7680
+	dw 5632, -7680
+	dw 5632, -6400
+	dw 5632, -5632
+	dw 5632, -5376
+	dw 4864, -4608
+	dw 4608, -4608
+	dw 4608, -4352
+	dw 3840, -4352
+	dw 3840, -4608
+	dw 3584, -4608
+	dw 3072, -4096
+	dw 2048, -4096
+	dw 1536, -4608
+	dw 1280, -4608
+	dw 1280, -4352
+	dw 512, -4352
+	dw 512, -4608
+	dw 256, -4608
+	dw -512, -5376
+	dw -512, -5632
+	dw -512, -6400
+	dw 0, -6400
+	dw 1536, -6400
+	dw 3584, -6400
+	dw 5120, -6400
+	dw 5120, -5632
+	dw 4608, -5120
+	dw 3584, -5120
+	dw 1536, -5120
+	dw 512, -5120
+	dw 0, -5632
+	dw 2304, -7168
+	dw 2304, -7680
+	dw 2304, -8192
+	dw 1792, -8192
+	dw 1280, -8192
+	dw 768, -8192
+	dw 256, -8192
+	dw -768, -8192
+	dw -768, -9216
+	dw -768, -9728
+	dw -768, -10240
+	dw -768, -11264
+	dw 256, -11264
+	dw 256, -10240
+	dw 256, -9728
+	dw 256, -9216
+	dw 768, -9216
+	dw 1280, -9216
+	dw 1792, -9216
+	dw 2304, -9216
+	dw 3328, -9216
+	dw 3328, -8192
+	dw 3328, -7680
+	dw 3328, -7168
+	dw -768, -12288
+	dw 256, -12288
+	dw 768, -12288
+	dw 2304, -10496
+	dw 3584, -10496
+	dw 4352, -10496
+	dw 5120, -11264
+	dw 5888, -11264
+	dw 5888, -10496
+	dw 5888, -8960
+	dw 7168, -8960
+	dw 7168, -7936
+	dw 5888, -7936
+	dw 3584, -7936
+	dw 3584, -9472
+	dw 2304, -9472
+	dw 1280, -9472
+	dw 768, -11264
+	dw 6144, -8960
+	dw 6144, -9472
+	dw 6144, -9984
+	dw 6144, -11264
+	dw 7168, -11264
+	dw 7680, -11264
+	dw 8192, -11264
+	dw 8704, -11264
+	dw 10240, -11264
+	dw 10240, -9984
+	dw 10240, -9472
+	dw 10240, -8960
+	dw 10240, -7936
+	dw 9472, -7936
+	dw 9472, -8448
+	dw 8704, -8448
+	dw 8704, -8960
+	dw 8704, -9472
+	dw 8704, -9984
+	dw 8192, -9984
+	dw 7680, -9984
+	dw 7168, -9984
+	dw 7168, -9472
+	dw 10240, -8448
+	dw 11008, -7936
+	dw 11776, -7936
+	dw 11776, -6400
+	dw 11776, -5632
+	dw 11776, -4864
+	dw 11776, -3072
+	dw 7936, -3072
+	dw 7936, -4864
+	dw 7936, -5632
+	dw 7936, -6400
+	dw 7936, -7936
+	dw 8704, -7936
+	dw 8704, -6400
+	dw 9472, -5632
+	dw 10240, -5632
+	dw 11008, -6400
+	dw 9472, -6656
+	dw 9728, -6400
+	dw 9984, -6400
+	dw 10240, -6656
+	dw 8704, -4864
+	dw 11008, -4864
+	dw 7424, -4864
+	dw 7424, -4352
+	dw 7936, -4352
+	dw 7936, -3584
+	dw 7424, -3584
+	dw 7424, -2560
+	dw 9472, -2560
+	dw 9472, -3072
+	dw 10240, -3072
+	dw 10240, -2560
+	dw 12032, -2560
+	dw 12544, -3072
+	dw 13056, -3072
+	dw 13056, -3328
+	dw 13824, -3328
+	dw 13824, -3072
+	dw 14336, -3072
+	dw 14848, -2560
+	dw 14848, -1280
+	dw 14336, -768
+	dw 13824, -768
+	dw 13056, -768
+	dw 12544, -768
+	dw 12032, -1280
+	dw 7424, -1280
+	dw 6144, -1280
+	dw 6144, -4864
+	dw 13056, -2304
+	dw 13824, -2304
+	dw 13824, -1536
+	dw 13056, -1536
+	dw 9728, -1280
+	dw 13056, -3584
+	dw 13056, -3840
+	dw 12032, -3840
+	dw 12032, -5888
+	dw 12032, -7936
+	dw 13568, -7936
+	dw 14848, -7936
+	dw 15360, -7936
+	dw 16128, -7936
+	dw 16640, -7424
+	dw 16640, -6912
+	dw 16128, -6144
+	dw 16128, -5632
+	dw 16640, -4864
+	dw 16640, -4352
+	dw 16128, -3840
+	dw 15360, -3840
+	dw 14848, -3840
+	dw 13824, -3840
+	dw 13824, -3584
+	dw 13568, -6400
+	dw 14848, -6400
+	dw 15360, -6144
+	dw 15360, -5632
+	dw 14848, -5376
+	dw 13568, -5376
+Vertices.Count:
+	dw 208 ;52 vertices
+
+Walls:
+Wall0:
+	db 7
+	db 32
+	db 0, 1
+	db 0, 0
+	dw 8
+Wall1:
+	db 7
+	db 64
+	db 1, 2
+	db 2, 2
+	dw 4
+Wall2:
+	db 3
+	db 0
+	db 2, 3
+	db 2, 8
+	dw 9
+Wall3:
+	db 7
+	db 192
+	db 3, 4
+	db 2, 2
+	dw 4
+Wall4:
+	db 7
+	db 224
+	db 4, 5
+	db 0, 0
+	dw 8
+Wall5:
+	db 5
+	db 0
+	db 5, 6
+	db 0, 0
+	dw 1
+Wall6:
+	db 4
+	db 0
+	db 6, 7
+	db 0, 0
+	dw 16
+Wall7:
+	db 6
+	db 0
+	db 7, 8
+	db 0, 0
+	dw 1
+Wall8:
+	db 7
+	db 32
+	db 8, 9
+	db 0, 0
+	dw 8
+Wall9:
+	db 7
+	db 64
+	db 9, 10
+	db 38, 38
+	dw 4
+Wall10:
+	db 3
+	db 0
+	db 10, 11
+	db 38, 9
+	dw 9
+Wall11:
+	db 7
+	db 192
+	db 11, 12
+	db 38, 38
+	dw 4
+Wall12:
+	db 7
+	db 224
+	db 12, 13
+	db 0, 0
+	dw 8
+Wall13:
+	db 7
+	db 192
+	db 13, 14
+	db 1, 1
+	dw 100
+Wall14:
+	db 4
+	db 160
+	db 14, 15
+	db 1, 1
+	dw 8
+Wall15:
+	db 5
+	db 128
+	db 15, 16
+	db 1, 1
+	dw 36
+Wall16:
+	db 4
+	db 128
+	db 16, 17
+	db 1, 1
+	dw 16
+Wall17:
+	db 6
+	db 128
+	db 17, 18
+	db 1, 1
+	dw 36
+Wall18:
+	db 4
+	db 96
+	db 18, 19
+	db 1, 1
+	dw 8
+Wall19:
+	db 7
+	db 64
+	db 19, 0
+	db 1, 1
+	dw 100
+Wall20:
+	db 3
+	db 0
+	db 1, 4
+	db 0, 2
+	dw 9
+Wall21:
+	db 3
+	db 128
+	db 4, 1
+	db 2, 0
+	dw 9
+Wall22:
+	db 3
+	db 0
+	db 9, 12
+	db 0, 38
+	dw 9
+Wall23:
+	db 3
+	db 128
+	db 12, 9
+	db 38, 0
+	dw 9
+Wall24:
+	db 3
+	db 230
+	db 0, 20
+	db 1, 0
+	dw 100
+Wall25:
+	db 3
+	db 26
+	db 21, 13
+	db 1, 0
+	dw 100
+Wall26:
+	db 3
+	db 128
+	db 21, 20
+	db 0, 3
+	dw 16
+Wall27:
+	db 3
+	db 128
+	db 23, 22
+	db 3, 4
+	dw 16
+Wall28:
+	db 3
+	db 128
+	db 25, 24
+	db 4, 1
+	dw 16
+Wall29:
+	db 1
+	db 64
+	db 24, 22
+	db 4, 1
+	dw 4
+Wall30:
+	db 2
+	db 64
+	db 22, 20
+	db 3, 1
+	dw 4
+Wall31:
+	db 1
+	db 192
+	db 21, 23
+	db 3, 1
+	dw 4
+Wall32:
+	db 2
+	db 192
+	db 23, 25
+	db 4, 1
+	dw 4
+Wall33:
+	db 7
+	db 0
+	db 26, 27
+	db 5, 5
+	dw 64
+Wall34:
+	db 7
+	db 192
+	db 27, 28
+	db 5, 5
+	dw 4
+Wall35:
+	db 7
+	db 64
+	db 29, 30
+	db 5, 5
+	dw 4
+Wall36:
+	db 7
+	db 0
+	db 30, 31
+	db 40, 40
+	dw 64
+Wall37:
+	db 5
+	db 192
+	db 31, 32
+	db 5, 5
+	dw 25
+Wall38:
+	db 4
+	db 192
+	db 32, 33
+	db 5, 5
+	dw 9
+Wall39:
+	db 6
+	db 192
+	db 33, 34
+	db 40, 40
+	dw 1
+Wall40:
+	db 7
+	db 160
+	db 34, 35
+	db 5, 5
+	dw 18
+Wall41:
+	db 7
+	db 128
+	db 35, 36
+	db 5, 5
+	dw 1
+Wall42:
+	db 7
+	db 192
+	db 36, 37
+	db 5, 5
+	dw 1
+Wall43:
+	db 7
+	db 192
+	db 37, 11
+	db 9, 9
+	dw 1
+Wall44:
+	db 7
+	db 64
+	db 10, 38
+	db 9, 9
+	dw 1
+Wall45:
+	db 7
+	db 64
+	db 38, 39
+	db 5, 5
+	dw 1
+Wall46:
+	db 7
+	db 128
+	db 39, 40
+	db 39, 39
+	dw 1
+Wall47:
+	db 7
+	db 160
+	db 40, 41
+	db 7, 7
+	dw 8
+Wall48:
+	db 7
+	db 128
+	db 41, 42
+	db 7, 7
+	dw 16
+Wall49:
+	db 7
+	db 96
+	db 42, 43
+	db 7, 7
+	dw 8
+Wall50:
+	db 3
+	db 128
+	db 40, 43
+	db 5, 7
+	dw 64
+Wall51:
+	db 7
+	db 128
+	db 43, 44
+	db 5, 5
+	dw 1
+Wall52:
+	db 7
+	db 192
+	db 44, 45
+	db 5, 5
+	dw 1
+Wall53:
+	db 7
+	db 192
+	db 45, 3
+	db 8, 8
+	dw 1
+Wall54:
+	db 7
+	db 64
+	db 2, 46
+	db 8, 8
+	dw 1
+Wall55:
+	db 7
+	db 64
+	db 46, 47
+	db 5, 5
+	dw 1
+Wall56:
+	db 7
+	db 128
+	db 47, 48
+	db 5, 5
+	dw 1
+Wall57:
+	db 7
+	db 96
+	db 48, 49
+	db 5, 5
+	dw 18
+Wall58:
+	db 5
+	db 64
+	db 49, 50
+	db 5, 5
+	dw 1
+Wall59:
+	db 4
+	db 64
+	db 50, 51
+	db 5, 5
+	dw 9
+Wall60:
+	db 6
+	db 64
+	db 51, 26
+	db 5, 5
+	dw 25
+Wall61:
+	db 1
+	db 0
+	db 52, 53
+	db 6, 5
+	dw 36
+Wall62:
+	db 0
+	db 0
+	db 53, 54
+	db 6, 5
+	dw 64
+Wall63:
+	db 2
+	db 0
+	db 54, 55
+	db 6, 5
+	dw 36
+Wall64:
+	db 3
+	db 192
+	db 55, 56
+	db 6, 5
+	dw 9
+Wall65:
+	db 3
+	db 160
+	db 56, 57
+	db 6, 5
+	dw 8
+Wall66:
+	db 1
+	db 128
+	db 57, 58
+	db 6, 39
+	dw 16
+Wall67:
+	db 0
+	db 128
+	db 58, 59
+	db 6, 5
+	dw 64
+Wall68:
+	db 2
+	db 128
+	db 59, 60
+	db 6, 5
+	dw 16
+Wall69:
+	db 3
+	db 96
+	db 60, 61
+	db 6, 5
+	dw 8
+Wall70:
+	db 3
+	db 64
+	db 61, 52
+	db 6, 5
+	dw 9
+Wall71:
+	db 3
+	db 0
+	db 38, 37
+	db 9, 5
+	dw 9
+Wall72:
+	db 3
+	db 0
+	db 46, 45
+	db 8, 5
+	dw 9
+Wall73:
+	db 5
+	db 64
+	db 62, 63
+	db 10, 10
+	dw 4
+Wall74:
+	db 6
+	db 64
+	db 63, 64
+	db 11, 11
+	dw 4
+Wall75:
+	db 5
+	db 128
+	db 64, 65
+	db 13, 13
+	dw 4
+Wall76:
+	db 4
+	db 128
+	db 65, 66
+	db 14, 14
+	dw 4
+Wall77:
+	db 4
+	db 128
+	db 66, 67
+	db 15, 15
+	dw 4
+Wall78:
+	db 4
+	db 128
+	db 67, 68
+	db 16, 16
+	dw 4
+Wall79:
+	db 6
+	db 128
+	db 68, 69
+	db 17, 17
+	dw 16
+Wall80:
+	db 5
+	db 64
+	db 69, 70
+	db 17, 17
+	dw 16
+Wall81:
+	db 4
+	db 64
+	db 70, 71
+	db 18, 18
+	dw 4
+Wall82:
+	db 4
+	db 64
+	db 71, 72
+	db 19, 19
+	dw 4
+Wall83:
+	db 6
+	db 64
+	db 72, 73
+	db 20, 20
+	dw 16
+Wall84:
+	db 3
+	db 0
+	db 73, 74
+	db 20, 21
+	dw 16
+Wall85:
+	db 5
+	db 192
+	db 74, 75
+	db 20, 20
+	dw 16
+Wall86:
+	db 4
+	db 192
+	db 75, 76
+	db 19, 19
+	dw 4
+Wall87:
+	db 6
+	db 192
+	db 76, 77
+	db 18, 18
+	dw 4
+Wall88:
+	db 5
+	db 0
+	db 77, 78
+	db 16, 16
+	dw 4
+Wall89:
+	db 4
+	db 0
+	db 78, 79
+	db 15, 15
+	dw 4
+Wall90:
+	db 4
+	db 0
+	db 79, 80
+	db 14, 14
+	dw 4
+Wall91:
+	db 4
+	db 0
+	db 80, 81
+	db 13, 13
+	dw 4
+Wall92:
+	db 6
+	db 0
+	db 81, 82
+	db 12, 12
+	dw 16
+Wall93:
+	db 5
+	db 192
+	db 82, 83
+	db 12, 12
+	dw 16
+Wall94:
+	db 4
+	db 192
+	db 83, 84
+	db 11, 11
+	dw 4
+Wall95:
+	db 6
+	db 192
+	db 84, 85
+	db 10, 10
+	dw 4
+Wall96:
+	db 0
+	db 0
+	db 62, 85
+	db 5, 10
+	dw 16
+Wall97:
+	db 3
+	db 0
+	db 63, 84
+	db 10, 11
+	dw 16
+Wall98:
+	db 3
+	db 0
+	db 64, 83
+	db 11, 12
+	dw 16
+Wall99:
+	db 3
+	db 64
+	db 64, 81
+	db 12, 13
+	dw 16
+Wall100:
+	db 3
+	db 64
+	db 65, 80
+	db 13, 14
+	dw 16
+Wall101:
+	db 3
+	db 64
+	db 66, 79
+	db 14, 15
+	dw 16
+Wall102:
+	db 3
+	db 64
+	db 67, 78
+	db 15, 16
+	dw 16
+Wall103:
+	db 3
+	db 64
+	db 68, 77
+	db 16, 17
+	dw 16
+Wall104:
+	db 3
+	db 0
+	db 70, 77
+	db 17, 18
+	dw 16
+Wall105:
+	db 3
+	db 0
+	db 71, 76
+	db 18, 19
+	dw 16
+Wall106:
+	db 3
+	db 0
+	db 72, 75
+	db 19, 20
+	dw 16
+Wall107:
+	db 7
+	db 0
+	db 28, 62
+	db 5, 5
+	dw 9
+Wall108:
+	db 7
+	db 0
+	db 85, 29
+	db 5, 5
+	dw 1
+Wall109:
+	db 7
+	db 64
+	db 73, 86
+	db 21, 21
+	dw 16
+Wall110:
+	db 5
+	db 0
+	db 86, 87
+	db 21, 21
+	dw 16
+Wall111:
+	db 6
+	db 0
+	db 87, 88
+	db 21, 21
+	dw 4
+Wall112:
+	db 7
+	db 221
+	db 88, 89
+	db 21, 21
+	dw 85
+Wall113:
+	db 5
+	db 0
+	db 89, 90
+	db 21, 21
+	dw 25
+Wall114:
+	db 6
+	db 0
+	db 90, 91
+	db 21, 21
+	dw 9
+Wall115:
+	db 7
+	db 32
+	db 91, 92
+	db 21, 21
+	dw 18
+Wall116:
+	db 7
+	db 0
+	db 92, 93
+	db 21, 21
+	dw 9
+Wall117:
+	db 5
+	db 192
+	db 93, 94
+	db 21, 21
+	dw 9
+Wall118:
+	db 6
+	db 192
+	db 94, 95
+	db 21, 21
+	dw 36
+Wall119:
+	db 7
+	db 0
+	db 95, 104
+	db 22, 22
+	dw 1
+Wall120:
+	db 6
+	db 192
+	db 96, 97
+	db 22, 22
+	dw 16
+Wall121:
+	db 5
+	db 128
+	db 97, 98
+	db 22, 22
+	dw 25
+Wall122:
+	db 6
+	db 128
+	db 98, 99
+	db 21, 21
+	dw 81
+Wall123:
+	db 7
+	db 64
+	db 99, 100
+	db 21, 21
+	dw 36
+Wall124:
+	db 5
+	db 128
+	db 100, 101
+	db 21, 21
+	dw 25
+Wall125:
+	db 6
+	db 128
+	db 101, 102
+	db 21, 21
+	dw 16
+Wall126:
+	db 7
+	db 75
+	db 102, 103
+	db 21, 21
+	dw 53
+Wall127:
+	db 7
+	db 128
+	db 103, 74
+	db 21, 21
+	dw 4
+Wall128:
+	db 2
+	db 192
+	db 95, 98
+	db 21, 22
+	dw 16
+Wall129:
+	db 5
+	db 64
+	db 104, 105
+	db 22, 22
+	dw 4
+Wall130:
+	db 4
+	db 64
+	db 105, 106
+	db 23, 23
+	dw 4
+Wall131:
+	db 6
+	db 64
+	db 106, 107
+	db 24, 24
+	dw 25
+Wall132:
+	db 5
+	db 0
+	db 107, 108
+	db 25, 25
+	dw 16
+Wall133:
+	db 4
+	db 0
+	db 108, 109
+	db 26, 26
+	dw 4
+Wall134:
+	db 4
+	db 0
+	db 109, 110
+	db 27, 27
+	dw 4
+Wall135:
+	db 4
+	db 0
+	db 110, 111
+	db 28, 28
+	dw 4
+Wall136:
+	db 6
+	db 0
+	db 111, 112
+	db 29, 29
+	dw 36
+Wall137:
+	db 5
+	db 192
+	db 112, 113
+	db 30, 30
+	dw 25
+Wall138:
+	db 4
+	db 192
+	db 113, 114
+	db 31, 31
+	dw 4
+Wall139:
+	db 4
+	db 192
+	db 114, 115
+	db 32, 32
+	dw 4
+Wall140:
+	db 4
+	db 192
+	db 115, 127
+	db 33, 33
+	dw 4
+Wall141:
+	db 3
+	db 128
+	db 116, 117
+	db 33, 34
+	dw 9
+Wall142:
+	db 7
+	db 64
+	db 117, 118
+	db 33, 33
+	dw 4
+Wall143:
+	db 7
+	db 128
+	db 118, 119
+	db 33, 33
+	dw 9
+Wall144:
+	db 5
+	db 64
+	db 119, 120
+	db 33, 33
+	dw 4
+Wall145:
+	db 4
+	db 64
+	db 120, 121
+	db 32, 32
+	dw 4
+Wall146:
+	db 6
+	db 64
+	db 121, 122
+	db 31, 31
+	dw 4
+Wall147:
+	db 5
+	db 128
+	db 122, 123
+	db 28, 28
+	dw 4
+Wall148:
+	db 4
+	db 128
+	db 123, 124
+	db 27, 27
+	dw 4
+Wall149:
+	db 6
+	db 128
+	db 124, 125
+	db 26, 26
+	dw 4
+Wall150:
+	db 5
+	db 192
+	db 125, 126
+	db 23, 23
+	dw 4
+Wall151:
+	db 4
+	db 192
+	db 126, 96
+	db 22, 22
+	dw 4
+Wall152:
+	db 3
+	db 0
+	db 105, 126
+	db 22, 23
+	dw 16
+Wall153:
+	db 3
+	db 0
+	db 106, 125
+	db 23, 24
+	dw 16
+Wall154:
+	db 3
+	db 220
+	db 107, 125
+	db 24, 25
+	dw 41
+Wall155:
+	db 3
+	db 192
+	db 108, 125
+	db 25, 26
+	dw 25
+Wall156:
+	db 3
+	db 192
+	db 109, 124
+	db 26, 27
+	dw 25
+Wall157:
+	db 3
+	db 192
+	db 110, 123
+	db 27, 28
+	dw 25
+Wall158:
+	db 3
+	db 192
+	db 111, 122
+	db 28, 29
+	dw 25
+Wall159:
+	db 3
+	db 157
+	db 112, 122
+	db 29, 30
+	dw 61
+Wall160:
+	db 3
+	db 128
+	db 113, 122
+	db 30, 31
+	dw 36
+Wall161:
+	db 3
+	db 128
+	db 114, 121
+	db 31, 32
+	dw 36
+Wall162:
+	db 3
+	db 128
+	db 115, 120
+	db 32, 33
+	dw 36
+Wall163:
+	db 7
+	db 192
+	db 127, 116
+	db 33, 33
+	dw 4
+Wall164:
+	db 5
+	db 0
+	db 138, 139
+	db 36, 36
+	dw 9
+Wall165:
+	db 6
+	db 0
+	db 139, 117
+	db 35, 35
+	dw 9
+Wall166:
+	db 5
+	db 0
+	db 116, 128
+	db 35, 35
+	dw 9
+Wall167:
+	db 6
+	db 0
+	db 128, 129
+	db 36, 36
+	dw 9
+Wall168:
+	db 5
+	db 192
+	db 129, 130
+	db 36, 36
+	dw 36
+Wall169:
+	db 4
+	db 192
+	db 130, 131
+	db 36, 36
+	dw 9
+Wall170:
+	db 4
+	db 192
+	db 131, 132
+	db 37, 37
+	dw 9
+Wall171:
+	db 6
+	db 192
+	db 132, 133
+	db 37, 37
+	dw 49
+Wall172:
+	db 7
+	db 128
+	db 133, 158
+	db 37, 37
+	dw 36
+Wall173:
+	db 0
+	db 64
+	db 153, 152
+	db 37, 43
+	dw 9
+Wall174:
+	db 4
+	db 64
+	db 135, 136
+	db 37, 37
+	dw 9
+Wall175:
+	db 4
+	db 64
+	db 136, 137
+	db 36, 36
+	dw 9
+Wall176:
+	db 6
+	db 64
+	db 137, 138
+	db 36, 36
+	dw 36
+Wall177:
+	db 3
+	db 192
+	db 139, 140
+	db 36, 35
+	dw 36
+Wall178:
+	db 3
+	db 224
+	db 140, 141
+	db 36, 35
+	dw 18
+Wall179:
+	db 3
+	db 0
+	db 141, 142
+	db 36, 35
+	dw 9
+Wall180:
+	db 3
+	db 32
+	db 142, 143
+	db 36, 35
+	dw 18
+Wall181:
+	db 3
+	db 64
+	db 143, 128
+	db 36, 35
+	dw 36
+Wall182:
+	db 3
+	db 192
+	db 117, 144
+	db 35, 34
+	dw 25
+Wall183:
+	db 3
+	db 224
+	db 144, 145
+	db 35, 34
+	dw 2
+Wall184:
+	db 3
+	db 0
+	db 145, 146
+	db 35, 34
+	dw 1
+Wall185:
+	db 3
+	db 32
+	db 146, 147
+	db 35, 34
+	dw 2
+Wall186:
+	db 3
+	db 64
+	db 147, 116
+	db 35, 34
+	dw 25
+Wall187:
+	db 3
+	db 224
+	db 136, 148
+	db 37, 36
+	dw 18
+Wall188:
+	db 3
+	db 0
+	db 148, 149
+	db 37, 36
+	dw 81
+Wall189:
+	db 3
+	db 32
+	db 149, 131
+	db 37, 36
+	dw 18
+Wall190:
+	db 0
+	db 128
+	db 158, 157
+	db 37, 43
+	dw 9
+Wall191:
+	db 7
+	db 128
+	db 157, 134
+	db 37, 37
+	dw 36
+Wall192:
+	db 7
+	db 64
+	db 134, 153
+	db 37, 37
+	dw 4
+Wall193:
+	db 5
+	db 64
+	db 152, 135
+	db 37, 37
+	dw 4
+Wall194:
+	db 7
+	db 0
+	db 151, 152
+	db 43, 43
+	dw 4
+Wall195:
+	db 7
+	db 128
+	db 153, 154
+	db 43, 43
+	dw 4
+Wall196:
+	db 3
+	db 192
+	db 151, 154
+	db 41, 43
+	dw 9
+Wall197:
+	db 7
+	db 192
+	db 154, 155
+	db 41, 41
+	dw 16
+Wall198:
+	db 7
+	db 0
+	db 155, 156
+	db 41, 41
+	dw 64
+Wall199:
+	db 7
+	db 64
+	db 156, 157
+	db 43, 43
+	dw 4
+Wall200:
+	db 7
+	db 192
+	db 158, 159
+	db 43, 43
+	dw 4
+Wall201:
+	db 3
+	db 0
+	db 156, 159
+	db 41, 43
+	dw 9
+Wall202:
+	db 7
+	db 0
+	db 159, 160
+	db 41, 41
+	dw 49
+Wall203:
+	db 7
+	db 32
+	db 160, 161
+	db 41, 41
+	dw 8
+Wall204:
+	db 7
+	db 0
+	db 161, 162
+	db 41, 41
+	dw 4
+Wall205:
+	db 5
+	db 64
+	db 162, 163
+	db 41, 41
+	dw 1
+Wall206:
+	db 3
+	db 0
+	db 163, 164
+	db 41, 44
+	dw 9
+Wall207:
+	db 6
+	db 192
+	db 164, 165
+	db 41, 41
+	dw 1
+Wall208:
+	db 7
+	db 0
+	db 165, 166
+	db 41, 41
+	dw 4
+Wall209:
+	db 7
+	db 224
+	db 166, 167
+	db 41, 41
+	dw 8
+Wall210:
+	db 7
+	db 192
+	db 167, 168
+	db 41, 41
+	dw 25
+Wall211:
+	db 7
+	db 160
+	db 168, 169
+	db 41, 41
+	dw 8
+Wall212:
+	db 5
+	db 128
+	db 169, 170
+	db 41, 41
+	dw 4
+Wall213:
+	db 4
+	db 128
+	db 170, 171
+	db 41, 41
+	dw 9
+Wall214:
+	db 6
+	db 128
+	db 171, 172
+	db 41, 41
+	dw 4
+Wall215:
+	db 7
+	db 96
+	db 172, 173
+	db 41, 41
+	dw 8
+Wall216:
+	db 5
+	db 128
+	db 173, 181
+	db 41, 41
+	dw 81
+Wall217:
+	db 6
+	db 128
+	db 174, 175
+	db 41, 41
+	dw 25
+Wall218:
+	db 7
+	db 64
+	db 175, 176
+	db 41, 41
+	dw 196
+Wall219:
+	db 7
+	db 0
+	db 176, 150
+	db 41, 41
+	dw 25
+Wall220:
+	db 7
+	db 192
+	db 150, 151
+	db 41, 41
+	dw 4
+Wall221:
+	db 3
+	db 0
+	db 177, 178
+	db 42, 41
+	dw 9
+Wall222:
+	db 3
+	db 192
+	db 178, 179
+	db 42, 41
+	dw 9
+Wall223:
+	db 3
+	db 128
+	db 179, 180
+	db 42, 41
+	dw 9
+Wall224:
+	db 3
+	db 64
+	db 180, 177
+	db 42, 41
+	dw 9
+Wall225:
+	db 4
+	db 128
+	db 181, 174
+	db 41, 41
+	dw 81
+Wall226:
+	db 4
+	db 64
+	db 163, 182
+	db 44, 44
+	dw 1
+Wall227:
+	db 4
+	db 192
+	db 201, 164
+	db 44, 44
+	dw 1
+Wall228:
+	db 6
+	db 64
+	db 182, 183
+	db 45, 45
+	dw 1
+Wall229:
+	db 5
+	db 192
+	db 200, 201
+	db 45, 45
+	dw 1
+Wall230:
+	db 7
+	db 128
+	db 183, 184
+	db 45, 45
+	dw 16
+Wall231:
+	db 5
+	db 64
+	db 184, 185
+	db 45, 45
+	dw 64
+Wall232:
+	db 6
+	db 64
+	db 185, 186
+	db 45, 45
+	dw 64
+Wall233:
+	db 5
+	db 0
+	db 186, 187
+	db 45, 45
+	dw 36
+Wall234:
+	db 4
+	db 0
+	db 187, 188
+	db 45, 45
+	dw 25
+Wall235:
+	db 4
+	db 0
+	db 188, 189
+	db 45, 45
+	dw 4
+Wall236:
+	db 6
+	db 0
+	db 189, 190
+	db 45, 45
+	dw 9
+Wall237:
+	db 7
+	db 224
+	db 190, 191
+	db 45, 45
+	dw 8
+Wall238:
+	db 7
+	db 192
+	db 191, 192
+	db 45, 45
+	dw 4
+Wall239:
+	db 7
+	db 169
+	db 192, 193
+	db 45, 45
+	dw 13
+Wall240:
+	db 7
+	db 192
+	db 193, 194
+	db 46, 46
+	dw 4
+Wall241:
+	db 7
+	db 216
+	db 194, 195
+	db 45, 45
+	dw 13
+Wall242:
+	db 7
+	db 192
+	db 195, 196
+	db 45, 45
+	dw 4
+Wall243:
+	db 7
+	db 160
+	db 196, 197
+	db 45, 45
+	dw 8
+Wall244:
+	db 5
+	db 128
+	db 197, 198
+	db 45, 45
+	dw 9
+Wall245:
+	db 4
+	db 128
+	db 198, 199
+	db 45, 45
+	dw 4
+Wall246:
+	db 6
+	db 128
+	db 199, 200
+	db 45, 45
+	dw 16
+Wall247:
+	db 7
+	db 128
+	db 203, 202
+	db 45, 45
+	dw 25
+Wall248:
+	db 7
+	db 109
+	db 204, 203
+	db 45, 45
+	dw 5
+Wall249:
+	db 3
+	db 0
+	db 204, 193
+	db 46, 45
+	dw 9
+Wall250:
+	db 3
+	db 128
+	db 194, 205
+	db 46, 45
+	dw 9
+Wall251:
+	db 7
+	db 18
+	db 206, 205
+	db 45, 45
+	dw 5
+Wall252:
+	db 7
+	db 0
+	db 207, 206
+	db 45, 45
+	dw 25
+Wall253:
+	db 7
+	db 192
+	db 202, 207
+	db 45, 45
+	dw 16
+Wall254:
+	db 3
+	db 0
+	db 182, 201
+	db 44, 45
+	dw 9
+Wall255:
+	db 7
+	db 64
+	db 205, 204
+	db 46, 46
+	dw 4
+Walls.Count:
+	dw 255
+
+Sectors:
+Sector0:  dw -128, 256
+Sector1:  dw 0, 256
+Sector2:  dw -128, 64
+Sector3:  dw -86, 256
+Sector4:  dw -42, 256
+Sector5:  dw -128, 64
+Sector6:  dw -160, 64
+Sector7:  dw -96, 64
+Sector8:  dw -128, -128
+Sector9:  dw -128, -128
+Sector10: dw -128, 24
+Sector11: dw -152, 24
+Sector12: dw -176, 24
+Sector13: dw -200, 24
+Sector14: dw -224, 24
+Sector15: dw -248, 24
+Sector16: dw -272, 24
+Sector17: dw -296, 24
+Sector18: dw -320, 24
+Sector19: dw -344, 24
+Sector20: dw -368, 24
+Sector21: dw -368, -112
+Sector22: dw -368, -176
+Sector23: dw -344, -80
+Sector24: dw -320, -80
+Sector25: dw -296, -80
+Sector26: dw -272, 16
+Sector27: dw -248, 16
+Sector28: dw -224, 16
+Sector29: dw -200, 16
+Sector30: dw -176, 16
+Sector31: dw -152, 112
+Sector32: dw -128, 112
+Sector33: dw -104, 112
+Sector34: dw -104, 256
+Sector35: dw -320, 256
+Sector36: dw -352, 256
+Sector37: dw -384, 256
+Sector38: dw -128, 64
+Sector39: dw -128, 64
+Sector40: dw -128, 64
+Sector41: dw -320, -128
+Sector42: dw -320, -96
+Sector43: dw -352, -128
+Sector44: dw -352, -112
+Sector45: dw -384, -96
+Sector46: dw -334, -256
+
+
