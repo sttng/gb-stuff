ad0f7d095c7edba15df55efda49977584554750d
sttng <46765778+sttng@users.noreply.github.com>
2025-03-09 16:45:32 +0800

    Update Vertices.asm

--- asm/Vertices.asm
+++ asm/Vertices.asm
@@ -11,7 +11,7 @@ Transform.Begin:
 
 	; Get sin(a)
 	ld a,[Camera_Angle]
-	;call Maths.Trig.Sin
+	call Maths.Trig.Sin
 	ld a, b
 	ld [SinA],a
 	ld a, c
@@ -19,7 +19,7 @@ Transform.Begin:
 
 	; Get cos(a)
 	ld a,[Camera_Angle]
-	;call Maths.Trig.Cos
+	call Maths.Trig.Cos
 	ld a, b
 	ld [CosA],a
 	ld a, c
@@ -63,3 +63,43 @@ Transform.Begin:
 	ld [Neg_Camera_Y+1],a
 
 	ret
+
+; ==========================================================================
+; Transform
+; --------------------------------------------------------------------------
+; Transforms a single vertex.
+; --------------------------------------------------------------------------
+; Inputs:    (BC,DE) Vertex to transform.
+; Outputs:   (BC,DE) Transformed vertex.
+; Destroyed: AF, BC, DE, HL.
+; ==========================================================================
+
+Transform:
+
+;NegCameraX = $+1
+	ld hl,$0000
+	add hl,bc
+	ld a,h
+	ld [Original_X],a
+	ld a,l
+	ld [Original_X + 1],a
+
+;NegCameraY = $+1
+	ld hl,$0000
+	add hl,de
+	ld a,h
+	ld [Original_Y],a
+	ld a,l
+	ld [Original_Y + 1],a
+
+	ld hl,SinA
+	ld a,[hl+]
+	ld h,[hl]
+	ld l,a
+	ld d,h
+	ld e,l
+
+;OriginalY = $+1
+	ld bc,0
+	call Maths.Mul.S16S16
+
