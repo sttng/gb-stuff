ad3104577c4e5114bc39d70d44a544c2256593a3
sttng <46765778+sttng@users.noreply.github.com>
2026-05-01 11:01:59 +0200

    Add files via upload

---
+++ asm/bresenham/build.bat
@@ -0,0 +1,29 @@
+@echo off
+
+rem Simple batch file to assemble Game Boy ROMs
+rem usage: build.bat <sourcefile>
+
+setlocal
+set _fn=%~n1
+
+if exist %_fn%.gb del %_fn%.gb
+
+echo Assembling...
+..\rgbasm -Weverything -o%_fn%.o %1
+if ERRORLEVEL 1 goto :error
+echo Linking...
+..\rgblink -w -t -n%_fn%.sym -m%_fn%.map -o %_fn%.gb %_fn%.o
+if ERRORLEVEL 1 goto :error
+echo Fixing...
+..\rgbfix -p 255 -v %_fn%.gb
+if ERRORLEVEL 1 goto :error
+
+echo Created: %_fn%.gb
+del *.o
+goto :end
+
+:error
+echo Build failed.
+
+:end
+endlocal
